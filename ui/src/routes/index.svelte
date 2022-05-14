b9559b08d733a48a181d2521c0e44049ce138824
Anthony Scinocco <scinocco.a@gmail.com>
2022-05-13 23:33:30 -0400

    Establish ws conn in root svelte file

--- ui/src/routes/index.svelte
+++ ui/src/routes/index.svelte
@@ -1,3 +1,28 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    let socket = new WebSocket("ws://localhost:8080/ws")
+
+    socket.onopen = () => {
+      console.log("connected");
+      socket.send("hi from client");
+    }
+    
+    socket.onclose = (e) => {
+      console.log("socket closed", e)
+    }
+    
+    socket.onerror = (err) => {
+      console.log("socket error", err);
+    }
+    
+    socket.onmessage = (msg) => {
+      console.log("message recieved", msg)
+    }
+  })
+</script>
+
 <h1 class="text-3xl font-bold underline">
   Hello world!
 </h1>
