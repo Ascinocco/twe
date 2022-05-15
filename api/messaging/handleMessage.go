b06202fdc65454f65f4284b74274ea8e18e460b0
Anthony Scinocco <scinocco.a@gmail.com>
2022-05-15 01:52:16 -0400

    add BE todos

--- api/messaging/handleMessage.go
+++ api/messaging/handleMessage.go
@@ -15,6 +15,9 @@ type HelloResponse struct {
 	Message string `json:"message"`
 }
 
+// @TODO: figure out how to structure handling messages
+// @TODO: Handle getting current user
+// @TODO: Handle getting active pmc
 func HandleMsg(msg Msg) {
 	switch msg.msgType {
 	case "hello":
